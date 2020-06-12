f6ca1940fb1d4f063a9deef361e8c4003fe31b4a
jaredhennessy <jared.hennessy@gmail.com>
2020-06-11 22:18:35 -0700

    fixed seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,25 @@
-DELETE FROM users WHERE firstName = 'Customer';
-DELETE FROM users WHERE firstName = 'Dev';
-INSERT users (firstName, lastName, email, password, createdAt, updatedAt) VALUES ('Customer','User','jared.hennessy+cust@gmail.com','password1234',CURDATE(),CURDATE());
-INSERT users (firstName, lastName, email, password, createdAt, updatedAt) VALUES ('Dev','User','jared.hennessy+dev@gmail.com','password5678',CURDATE(),CURDATE());
+use y25qno7e1qf1ncyh;
+/*
+drop table Tasks;
+drop table Users;
+*/
 
-TRUNCATE TABLE tasks;
+DELETE FROM Users WHERE firstName = 'Customer';
+DELETE FROM Users WHERE firstName = 'Dev';
+INSERT Users (firstName, lastName, email, password, createdAt, updatedAt) VALUES ('Customer','User','jared.hennessy+cust@gmail.com','password1234',CURDATE(),CURDATE());
+INSERT Users (firstName, lastName, email, password, createdAt, updatedAt) VALUES ('Dev','User','jared.hennessy+dev@gmail.com','password5678',CURDATE(),CURDATE());
 
-INSERT tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
-VALUES ('task API routes','complete get, post, put via sequelize',false,CURDATE(),CURDATE(),CURDATE(),(select id from users where firstName = 'Dev'),(select id from users where firstName = 'Customer'));
+TRUNCATE TABLE Tasks;
 
-INSERT tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
-VALUES ('configure passport','signup/login/logout',true,'2020-06-09',CURDATE(),CURDATE(),(select id from users where firstName = 'Dev'),(select id from users where firstName = 'Customer'));
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('task API routes','complete get, post, put via sequelize',false,CURDATE(),CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
 
-INSERT tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
-VALUES ('seeds','populate tables',true,'2020-06-12',CURDATE(),CURDATE(),NULL,(select id from users where firstName = 'Customer'));
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('configure passport','signup/login/logout',true,'2020-06-09',CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
 
-select * from users;
-select * from tasks;
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('seeds','populate tables',true,'2020-06-12',CURDATE(),CURDATE(),NULL,(select id from Users where firstName = 'Customer'));
+
+select * from Users;
+select * from Tasks;
 
