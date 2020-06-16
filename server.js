c68a30460f4f71d2fc24375890ff03703d06d93e
jaredhennessy <jared.hennessy@gmail.com>
2020-06-16 13:32:32 -0700

    troubleshooting heroku build

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/"));
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({
