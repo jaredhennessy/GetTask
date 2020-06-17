af021b4d505c32ade944403f2cca9157635847c4
jaredhennessy <jared.hennessy@gmail.com>
2020-06-16 20:55:33 -0700

    made some buttons work

--- public/assets/js/task.js
+++ public/assets/js/task.js
@@ -1,6 +1,12 @@
-//===============Calendar plugin===================//
-document.addEventListener("DOMContentLoaded", () => {
-  const elems = document.querySelectorAll(".datepicker");
-  const instances = M.Datepicker.init(elems);
-  console.log(instances);
+$(document).ready(() => {
+  //===============Calendar plugin===================//
+  document.addEventListener("DOMContentLoaded", () => {
+    const elems = document.querySelectorAll(".datepicker");
+    const instances = M.Datepicker.init(elems);
+    console.log(instances);
+  });
+
+  $("#btnBack").on("click", () => {
+    $(location).attr("href", "/list");
+  });
 });
