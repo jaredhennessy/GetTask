660d7c725e379ffa3f45a2c502a650f57274aab9
jaredhennessy <jared.hennessy@gmail.com>
2020-06-16 18:15:13 -0700

    more tasks in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,13 +12,25 @@ INSERT Users (firstName, lastName, email, password, createdAt, updatedAt) VALUES
 TRUNCATE TABLE Tasks;
 
 INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
-VALUES ('task API routes','complete get, post, put via sequelize',true,CURDATE(),CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
+VALUES ('task API routes','complete get, post, put via sequelize',false,CURDATE(),CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
 
 INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
 VALUES ('configure passport','signup/login/logout',false,'2020-06-09',CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
 
 INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
-VALUES ('seeds','populate tables',false,'2020-06-12',CURDATE(),CURDATE(),NULL,(select id from Users where firstName = 'Customer'));
+VALUES ('user list','build users.handlebars',false,'2020-06-17',CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
+
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('task links','link to update page task.handlebars from rows in list.handlebars',false,'2020-06-18',CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
+
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('stale tickets','Color-code expired and about-to-expire tasks',false,'2020-06-19',CURDATE(),CURDATE(),(select id from Users where firstName = 'Dev'),(select id from Users where firstName = 'Customer'));
+
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('seeds','populate tables',true,'2020-06-12',CURDATE(),CURDATE(),NULL,(select id from Users where firstName = 'Customer'));
+
+INSERT Tasks (title, description, complete, estCompletion, createdAt, updatedAt, assigneeId, creatorId)
+VALUES ('email alerts','configure an email alert when ticket is assigned',false,'2020-07-01',CURDATE(),CURDATE(),NULL,(select id from Users where firstName = 'Customer'));
 
 select * from Users;
 select * from Tasks;
