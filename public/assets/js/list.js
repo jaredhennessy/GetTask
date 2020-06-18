a1a69cbf7ee4dd5d74b378c1a892cba202fffb95
Tyler McBride <tymcbrid@gmail.com>
2020-06-17 21:33:56 -0700

    list fix hopefully

--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -9,13 +9,13 @@ $(document).ready(() => {
     event.preventDefault();
     // console.log(event.currentTarget.children[1]);
     // console.log(event.currentTarget.childNodes[3]);
-    console.log(event.currentTarget.children[1].firstChild.val);
-    console.log(event.currentTarget.childNodes[3].firstChild.value);
-    console.log(event);
-    // const taskId = $(this).data("id");
+    console.log(event.currentTarget.children[1].firstChild.textContent);
+    // console.log(event.currentTarget.childNodes[3].firstChild.value);
+    // console.log(event);
+    const taskId = event.currentTarget.children[1].firstChild.textContent;
     // const taskId = $("#task-id").text();
     // const taskId2 = $(this);
-    // $(location).attr("href", "/task/" + taskId);
+    $(location).attr("href", "/task/" + taskId);
   });
 
   $("#btnNew").on("click", () => {
